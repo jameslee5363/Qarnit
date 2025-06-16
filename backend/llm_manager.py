e55def46e646ef5358580fb25f16af9541698ef5
Eunice Pak <eunicepak18@gmail.com>
2025-06-16 16:36:24 -0400

    Update llm_manager.py

--- backend/llm_manager.py
+++ backend/llm_manager.py
@@ -4,9 +4,9 @@ from langchain_openai import AzureChatOpenAI
 class LLMManager:
     def __init__(self):
         self.llm = AzureChatOpenAI(
-            azure_endpoint="...",
-            api_key="...",
-            api_version="..."
+            azure_endpoint="https://qarnit-lucia-demo.openai.azure.com/openai/deployments/gpt-4.1-nano/chat/completions?api-version=2025-01-01-preview",
+            api_key="xfnUma2MdNuO3HlkjbtxVTurM7aTzIzJqZFs7EuwOA7EYzuMDps0JQQJ99BFACHYHv6XJ3w3AAABACOGeyWV",
+            api_version="2025-04-14"
         )
 
     def invoke(self, prompt, **kwargs) -> str:
